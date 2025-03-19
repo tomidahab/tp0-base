595e0d600e2a8a5bcaf1e8fbc3cbc0b2ed4e21b4
Tomas Dahab <tomidahab@gmail.com>
2025-03-19 13:57:11 -0300

    added to .sh instead of python

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
-echo "Nombre del archivo de salida: $1"
-echo "Cantidad de clientes: $2"
-python3 mi-generador.py $1 $2
 
-exit 0
+if [ $# -ne 2 ]; then
+    echo "Error en los argumentos. Uso: ./generar_docker_compose.sh <archivo_salida> <n_clients>"
+    exit 1
+fi
+
+nombre=$1
+n_clients=$2
+
+cat <<EOL > $nombre
+version: '1.0'
+name: tp0
+
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+
+services:
+  server:
+    container_name: server
+    entrypoint: python /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    image: server:latest
+    networks:
+      - testing_net
+
+EOL
+
+for i in $(seq 1 $n_clients); do
+    cat <<EOL >> $nombre
+  client$i:
+    container_name: client$i
+    depends_on:
+      - server
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    image: client:latest
+    networks:
+      - testing_net
+
+EOL
+done
+
+echo "Archivo $nombre generado exitosamente."
