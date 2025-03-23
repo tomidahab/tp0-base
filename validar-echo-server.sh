787157f23cccd35bd7ed13635d7803cd2b4efecc
Tomas Dahab <tomidahab@gmail.com>
2025-03-23 15:47:53 -0300

    edit server image to test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,9 +4,9 @@ SERVER_CONTAINER="server"
 MESSAGE="Hello World!"
 SERVER_PORT=12345
 
-docker network create validate_network
+docker network inspect validate_network >/dev/null 2>&1 || docker network create validate_network
 
-docker run -d --rm --name server --network validate_network echo-server-image
+docker run -d --rm --name server --network validate_network server:latest
 
 sleep 2
 
