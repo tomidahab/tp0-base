63e84447fed280eda5a66df7bc35fb873116b15a
Tomas Dahab <tomidahab@gmail.com>
2025-03-23 15:54:03 -0300

    .

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-SERVER_CONTAINER="server"
 MESSAGE="Hello World!"
 SERVER_PORT=12345
 
 docker network inspect validate_network >/dev/null 2>&1 || docker network create validate_network
 
-if ! docker ps -q -f name="$SERVER_CONTAINER" >/dev/null; then
-    docker run -d --rm --name "$SERVER_CONTAINER" --network "$NETWORK_NAME" server:latest
+if ! docker ps -q -f name=server >/dev/null; then
+    docker run -d --rm --name server --network "$NETWORK_NAME" server:latest
 fi
 
 sleep 2
